8879254fd3be907abe6308c3f5e477053eaf7fb7
IdrisSyaifulloh <saefullahiddris@gmail.com>
2026-04-29 11:48:23 +0700

    Fix:Photo

--- src/app/components/ProfileAvatar.tsx
+++ src/app/components/ProfileAvatar.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Avatar, AvatarFallback, AvatarImage } from "./Avatar";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 
 type ProfileAvatarProps = {
   name?: string | null;
